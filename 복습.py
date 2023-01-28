57252373d3e2d3a5fce9767db2d92d338242d64f
zogam02 <zogam022009@gmail.com>
2023-01-28 10:32:54 +0900

    반복문 복습

---
+++ 복습.py
@@ -0,0 +1,35 @@
+# 1
+# i=100
+# while i<110:
+#     if i%2==1:
+#         print(i)
+#     i+=1
+
+# for i in range(100,111):
+#      if i%2==1:
+#         print(i)
+    
+# 2
+# i=100
+# while i<201:
+#     if i%2==0:
+#         if i%3==0:
+#             if i%5==0:
+#                 print(i)
+#     i+=1
+
+# for i in range(100,201):
+#     if i%2==0:
+#         if i%3==0:
+#             if i%5==0:
+#                 print(i)
+# 3
+i=1
+mul=0
+while i<11:
+    if i%3!=0:
+        mul+=i
+    i+=1
+print(mul)
+for i in range
+    
