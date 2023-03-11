b497080868c1c83b6964576a1e13e953c7764ceb
zogam02 <zogam022009@gmail.com>
2023-03-11 11:32:04 +0900

    dppppppppppp

---
+++ day7/qhrtmq.py
@@ -0,0 +1,14 @@
+# import matplotlib.pyplot as plt
+# plt.figure()
+# plt.plot([1,0],[0,-1],[-1,0],[0,1])
+# plt.xlabel("x")
+# plt.ylabel("y")
+# plt.title("y=x+1,y=x-1")
+# plt.show()
+import matplotlib.pyplot as plt
+plt.figure()
+plt.plot([-1,0],[0,1],[-1,0],[0,-1],[1,0],[0,-1],[1,0],[0,1])
+plt.xlabel("x")
+plt.ylabel("y")
+plt.title("dppppp")
+plt.show()
