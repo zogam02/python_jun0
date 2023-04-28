0c08d83f450524acc931585814f9828b87b2e29c
zogam02 <zogam022009@gmail.com>
2023-04-29 00:18:29 +0900

    hw

---
+++ day9/hwdppppppppppppppp.py
@@ -0,0 +1,5 @@
+for i in range(5):   
+    num1=input("첫번째 수는?: ")
+    num2=input("두번째 수는?: ")
+    print(f"첫번째 수는{num1}이고 두번째수는{num2}이다")
+    print(f"두 수의 합은{int(num1)+int(num2)}이고 두수의 차는{int(num1)-int(num2)}이고 곱하기는{int(num1)*int(num2)}이다 나누기는 몫은{int(num1)/int(num2)}이고 나머지는{int(num1)%int(num2)}이다")
