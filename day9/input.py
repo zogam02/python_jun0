fbf67b26ade1da4dddb107bc47b2f60616d6a657
zogam02 <zogam022009@gmail.com>
2023-04-29 00:18:39 +0900

    hw

---
+++ day9/input.py
@@ -0,0 +1,17 @@
+# for i in range(3):
+#     str=input("여기에 입력해 주세여:")
+#     print(f"당신이 입력한 내용은'{str}'엥입ㄴㅣㄷㅏ ㅎㅠㅁㅓㄴ")
+
+
+
+
+
+for i in range(5):
+    animal=input("좋아하는 동물은?:")
+    fruit=input("좋아하는 과일은?:")
+    print(f"동물={animal},과일={fruit}")
+
+for i in range(5):
+    num1=input("1st수는?:")
+    num2=input("2nd수는?:")
+    print(f"두 수의 합은{int(num1)+int(num2)}이다")
